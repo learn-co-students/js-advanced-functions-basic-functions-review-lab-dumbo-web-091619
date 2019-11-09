6322fde62539692c421534903e2ca408d2b22b70
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-09 14:49:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let saturdayFun = (activity = "roller-skate") => {
+const saturdayFun = (activity = "roller-skate") => {
     return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = (activity = "go to the office") => {
+const mondayWork = (activity = "go to the office") => {
     return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = (stringParam = "*") => {
+const wrapAdjective = (stringParam = "*") => {
     return (adjParam = "special") => {
         return `You are ${stringParam}${adjParam}${stringParam}!`
     }
@@ -19,7 +19,7 @@ const Calculator = {
     divide: (num1, num2) => {return num1 / num2}
 }
 
-let actionApplyer = (num, functionsArray) => {
+const actionApplyer = (num, functionsArray) => {
    functionsArray.forEach(func => {
        num = func(num)
    })
